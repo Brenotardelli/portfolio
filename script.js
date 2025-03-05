f92d5da9497ec492627f854ac76af6a804ed8622
Breno Tardelli <brenotardelli@brenos-macbook-pro.home>
2025-03-05 23:39:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function menuMobile() {
+  const mobileMenu = document.querySelector(".btn-hamb");
+  const navList = document.querySelector(".nav-list");
+
+  mobileMenu.addEventListener("click", () => {
+    const isActive = navList.classList.contains("active");
+    navList.classList.toggle("active", !isActive);
+    mobileMenu.classList.toggle("active", !isActive);
+  });
+}
+menuMobile();

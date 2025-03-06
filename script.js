9e042d62edcf4a7c35dc10b914b9820a385dc5c4
Breno Tardelli <brenotardelli@brenos-macbook-pro.home>
2025-03-06 12:46:43 +0100

    portfolio almost completd

--- script.js
+++ script.js
@@ -11,3 +11,42 @@ function menuMobile() {
   });
 }
 menuMobile();
+
+function scrollSmooth() {
+  const links = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  links.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+scrollSmooth();
+
+function animaScroll() {
+  const content = document.querySelectorAll(".anima-scroll");
+
+  window.addEventListener("scroll", checkContent);
+
+  function checkContent() {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+
+    content.forEach((box) => {
+      const contentTop = box.getBoundingClientRect().top;
+
+      if (contentTop < triggerBottom) {
+        box.classList.add("show");
+      }
+    });
+  }
+}
+animaScroll();
